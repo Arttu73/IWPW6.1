deb0b6ef62eef451810bcd58e75efb8ba15ad83a
Arttu Laitinen <124583634+Arttu73@users.noreply.github.com>
2023-07-27 19:35:08 +0000

    Pull

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import "./styles.css";
 
-import { Chart } from "frappe-charts/dist/frappe-charts.min.esm";
+import { Chart } from "frappe-charts/dist/frappe-charts.min.esm"
 
 const queryJson = {
   query: [
@@ -75,31 +75,7 @@ const getData = async () => {
 const buildChart = async () => {
   const data = await getData();
 
-  const chart = new Chart("#chart", {
-    title: "Finnish parliamentary elections",
-    data: data,
-    type: "line",
-    height: 450,
-    colors: [
-      "#f54b4b",
-      "#ffde55",
-      "#006288",
-      "#349a2b",
-      "#61bf1a",
-      "#f00a64",
-      "#ffdd93",
-      "#0135a5"
-    ],
-    /*barOptions: {
-        stacked: 1
-    },*/
-    lineOptions: {
-      hideDots: 1,
-      regionFill: 0
-    }
-  });
 
-  /*
   const chart = new frappe.Chart("#chart", {
     title: "This is a title",
     data: data,
@@ -107,63 +83,5 @@ const buildChart = async () => {
     height: 450,
     colors: ["#eb5146"]
   });
-*/
+
 };
-
-new Chart("#chart", {
-  // or DOM element
-  data: {
-    labels: [
-      "12am-3am",
-      "3am-6am",
-      "6am-9am",
-      "9am-12pm",
-      "12pm-3pm",
-      "3pm-6pm",
-      "6pm-9pm",
-      "9pm-12am"
-    ],
-
-    datasets: [
-      {
-        name: "Some Data",
-        chartType: "bar",
-        values: [25, 40, 30, 35, 8, 52, 17, -4]
-      },
-      {
-        name: "Another Set",
-        chartType: "bar",
-        values: [25, 50, -10, 15, 18, 32, 27, 14]
-      },
-      {
-        name: "Yet Another",
-        chartType: "line",
-        values: [15, 20, -3, -15, 58, 12, -17, 37]
-      }
-    ],
-
-    yMarkers: [{ label: "Marker", value: 70, options: { labelPos: "left" } }],
-    yRegions: [
-      { label: "Region", start: -10, end: 50, options: { labelPos: "right" } }
-    ]
-  },
-
-  title: "My Awesome Chart",
-  type: "axis-mixed", // or 'bar', 'line', 'pie', 'percentage'
-  height: 300,
-  colors: ["purple", "#ffa3ef", "light-blue"],
-  axisOptions: {
-    xAxisMode: "tick",
-    xIsSeries: true
-  },
-  barOptions: {
-    stacked: true,
-    spaceRatio: 0.5
-  },
-  tooltipOptions: {
-    formatTooltipX: (d) => (d + "").toUpperCase(),
-    formatTooltipY: (d) => d + " pts"
-  }
-});
-
-buildChart();
